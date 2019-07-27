53439afaa0f70337c9d629f72c51b3cb97e6a5db
Banee Ishaque K <baneeishaque@gmail.com>
2019-07-27 17:07:35 +0530

    Initial : All Laptops

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ products = []  # List to store name of the product
 prices = []  # List to store price of the product
 ratings = []  # List to store rating of the product
 
-driver.get("https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+driver.get("https://www.flipkart.com/laptops/pr?sid=6bo,b5g&marketplace=FLIPKART")
 content = driver.page_source
 soup = BeautifulSoup(content)
 
@@ -21,7 +21,10 @@ for a in soup.findAll('a', href=True, attrs={'class': '_31qSD5'}):
     # print(rating.text)
     products.append(name.text)
     prices.append(price.text)
-    ratings.append(rating.text)
+    if rating is None:
+        ratings.append("-")
+    else:
+        ratings.append(rating.text)
 
 df = pd.DataFrame({'Product Name': products, 'Price': prices, 'Rating': ratings})
 df.to_csv('products.csv', index=False, encoding='utf-8')
